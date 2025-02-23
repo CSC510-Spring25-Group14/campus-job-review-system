4554a89d450f1b643319f4856838f27b24788b80
Mano Prakash Parthasarathi <mpartha@ncsu.edu>
2025-02-23 16:37:03 -0500

    exclude id while extracting experience summary

--- app/util.py
+++ app/util.py
@@ -19,6 +19,8 @@ def extract_experience_summary(applicant):
   # Combine all experiences into experience_summary
   for d in experience_list:
     for key, value in d.items():
+      if key == "id":
+        continue
       if key in experience_summary:
         if key in ['username']:
           continue
@@ -33,3 +35,35 @@ def extract_experience_summary(applicant):
           experience_summary[key] = [value]
   
   return experience_summary
+
+def call_groq_api(template_path, input_data):
+  """
+    Given a prompt template, the function returns the output from GROQ AI
+  """
+  with open(template_path, "r") as pt:
+    prompt_temp = pt.read()
+  
+  from groq import Groq
+  import os
+
+  client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+  completion = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+      {
+        "role": "user",
+        "content": prompt_temp
+      },
+      {
+        "role": "user",
+        "content": input_data
+      }
+    ],
+    temperature=0.4,
+    max_completion_tokens=1024,
+    top_p=1,
+    stream=False,
+    stop=None
+  )
+
+  return completion.choices[0].message.content
